8ee797f4521945918d95c3c2531033047dfa79fe
juan.c.fernandez <juan.carlos.fernandez@ricoh.es>
2024-07-30 15:38:34 +0200

    update script

--- scripts/update_vm_expiry_date.sh
+++ scripts/update_vm_expiry_date.sh
@@ -21,41 +21,48 @@ set_az_virtual_machine_expired_date() {
     fi
 
     # Get information about the virtual machine
-    TARGET_VM_INFO=$(az resource list --query "[?name=='$LAB_NAME/$VM_NAME' && type=='Microsoft.DevTestLab/labs/virtualMachines']" --output tsv)
+    TARGET_VM_INFO=$(az resource list --query "[?name=='$LAB_NAME/$VM_NAME' && type=='Microsoft.DevTestLab/labs/virtualMachines']" --output json)
 
     # If the VM is not found, throw an error
-    if [[ -z "$TARGET_VM_INFO" ]]; then
+    if [[ -z "$TARGET_VM_INFO" || "$TARGET_VM_INFO" == "[]" ]]; then
         echo "Error: No VM named $VM_NAME found in lab $LAB_NAME."
         exit 1
     fi
 
     # Get the resource ID of the virtual machine
-    RESOURCE_ID=$(echo $TARGET_VM_INFO | awk '{print $1}')
+    RESOURCE_ID=$(echo $TARGET_VM_INFO | jq -r '.[0].id')
 
     # Get the properties of the virtual machine
-    VM_PROPERTIES=$(az resource show --id $RESOURCE_ID --query properties)
+    VM_PROPERTIES=$(az resource show --id $RESOURCE_ID --query properties --output json)
 
     # Set the expiration date
     UPDATED_VM_PROPERTIES=$(echo $VM_PROPERTIES | jq --arg date "$EXPIRED_UTC_DATE" '.expirationDate = $date')
 
     # Update the resource with the new expiration date
-    az resource update --id $RESOURCE_ID --set properties="$UPDATED_VM_PROPERTIES"
+    az resource update --id $RESOURCE_ID --set properties="$(echo $UPDATED_VM_PROPERTIES | jq -c .)"
 
     echo "Successfully set VM '$LAB_NAME/$VM_NAME' to expire on UTC $EXPIRED_UTC_DATE"
 }
 
-# Log in to Azure
-az login --use-device-code
-
-# Set the VM name, lab name, and expiration date
-VM_NAME="vm-poc-noatum"
-LAB_NAME="devlab-poc-noatum"
-EXPIRED_UTC_DATE="2025-10-10"
-
-# Ensure parameters are provided and call the function
-if [[ -z "$VM_NAME" || -z "$LAB_NAME" || -z "$EXPIRED_UTC_DATE" ]]; then
-    echo "Error: VM_NAME, LAB_NAME, and EXPIRED_UTC_DATE parameters are required."
-    exit 1
+# Check if the user is already logged in
+if ! az account show > /dev/null 2>&1; then
+    echo "Logging in to Azure..."
+    az login --use-device-code
 else
-    set_az_virtual_machine_expired_date "$VM_NAME" "$LAB_NAME" "$EXPIRED_UTC_DATE"
+    echo "Already logged in to Azure."
 fi
+
+# Ensure three arguments are provided
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 <VM_NAME> <LAB_NAME> <EXPIRED_UTC_DATE>"
+    echo "Sample: $0 vm-poc-noatum devlab-poc-noatum 2025-10-10"
+    exit 1
+fi
+
+# Get command-line arguments
+VM_NAME=$1
+LAB_NAME=$2
+EXPIRED_UTC_DATE=$3
+
+# Call the function with the provided parameters
+set_az_virtual_machine_expired_date "$VM_NAME" "$LAB_NAME" "$EXPIRED_UTC_DATE"
